cb1327f2303846b8675bce554c10036fea80232e
Joseph Gentle <me@josephg.com>
2017-07-06 16:48:59 +1000

    Initial Commit

---
+++ minikafka/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  minikafka
+//
+//  Created by Joseph Gentle on 6/7/17.
+//  Copyright © 2017 Joseph Gentle. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
